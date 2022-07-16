66b69ac3d9ec7e8fbb735548a9711ec2a7ac3e8f
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-07-16 04:19:06 -0500

    test para fizzbuzz

--- test/FizzbuzzService.test.js
+++ test/FizzbuzzService.test.js
@@ -5,24 +5,20 @@ describe("Pruebas Unitarias para la clase FizzbuzzService", () =>{
 		const explorer1 = {name: "Explorer1", score: 1}
 		const resp1=FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1}
 		expect(resp1).toBeDefined()
-		//expect(resp1.trick).toContain('1')
 	});
 	test('Validando explorer 3', () => {
 		const explorer3 = {name: "Explorer3", score: 3}
 		const resp2=FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
 		expect(resp2).toBeDefined()
-		//expect(resp2.trick).toContain('FIZZ')
 	});
 	test('Validando explorer 5 y 15', () => {
 		const explorer5 = {name: "Explorer5", score: 5}
 		const resp3=FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
 		expect(resp3).toBeDefined()
-		//expect(resp3.trick).toContain('BUZZ')
 
 		const explorer15 = {name: "Explorer15", score: 15}
 		const resp4= FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
 		expect(resp4).toBeDefined()
-		//expect(resp4.trick).toContain('FIZZBUZZ')
 	});
 })
 
