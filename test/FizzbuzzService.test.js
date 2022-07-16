745c5d7ea1ca3c819de3c2aceb32af42d1486e0f
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-07-16 04:33:40 -0500

    agregar eslint

--- test/FizzbuzzService.test.js
+++ test/FizzbuzzService.test.js
@@ -1,26 +1,26 @@
-const FizzbuzzService = require("./../lib/services/FizzbuzzService")
+const FizzbuzzService = require("./../lib/services/FizzbuzzService");
 
 describe("Pruebas Unitarias para la clase FizzbuzzService", () =>{
-	test('Validando explorer 1', () => {
-		const explorer1 = {name: "Explorer1", score: 1}
-		const resp1=FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1}
-		expect(resp1).toBeDefined()
-	});
-	test('Validando explorer 3', () => {
-		const explorer3 = {name: "Explorer3", score: 3}
-		const resp2=FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
-		expect(resp2).toBeDefined()
-	});
-	test('Validando explorer 5 y 15', () => {
-		const explorer5 = {name: "Explorer5", score: 5}
-		const resp3=FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
-		expect(resp3).toBeDefined()
+    test("Validando explorer 1", () => {
+        const explorer1 = {name: "Explorer1", score: 1};
+        const resp1=FizzbuzzService.applyValidationInExplorer(explorer1); // {name: "Explorer1", score: 1, trick: 1}
+        expect(resp1).toBeDefined();
+    });
+    test("Validando explorer 3", () => {
+        const explorer3 = {name: "Explorer3", score: 3};
+        const resp2=FizzbuzzService.applyValidationInExplorer(explorer3); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        expect(resp2).toBeDefined();
+    });
+    test("Validando explorer 5 y 15", () => {
+        const explorer5 = {name: "Explorer5", score: 5};
+        const resp3=FizzbuzzService.applyValidationInExplorer(explorer5); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+        expect(resp3).toBeDefined();
 
-		const explorer15 = {name: "Explorer15", score: 15}
-		const resp4= FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
-		expect(resp4).toBeDefined()
-	});
-})
+        const explorer15 = {name: "Explorer15", score: 15};
+        const resp4= FizzbuzzService.applyValidationInExplorer(explorer15); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+        expect(resp4).toBeDefined();
+    });
+});
 
 
 
--- test/Reader.test.js
+++ test/Reader.test.js
@@ -1,10 +1,10 @@
 //correr prueba con 'npm test  <donde esta la prueba>'
-const Reader = require("./../lib/utils/Reader")
+const Reader = require("./../lib/utils/Reader");
 
 describe("Pruebas Unitarias para la clase Reader", () =>{
-	test('Leer un archivo JSON', () => {
-		const explorers = Reader.readJsonFile("explorers.json") // esto regresa la lista de explorers del archivo
-		expect(explorers).not.toBeNull()
-  		expect(explorers).toBeDefined()
-	});
-})
+    test("Leer un archivo JSON", () => {
+        const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+        expect(explorers).not.toBeNull();
+  		expect(explorers).toBeDefined();
+    });
+});
