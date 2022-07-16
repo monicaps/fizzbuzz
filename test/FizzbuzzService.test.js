5906247a9c1e7aa7bd3d53d6fd867421a6c0fc1f
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-07-16 04:11:52 -0500

    correccion final prueba fizzbuzz

--- test/FizzbuzzService.test.js
+++ test/FizzbuzzService.test.js
@@ -1,27 +1,27 @@
 const FizzbuzzService = require("./../lib/services/FizzbuzzService")
 
-describe("Pruebas Unitarias para la clase Reader", () =>{
+describe("Pruebas Unitarias para la clase FizzbuzzService", () =>{
 	test('Validando explorer 1', () => {
 		const explorer1 = {name: "Explorer1", score: 1}
 		const resp1=FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1}
-		expect(explorersMission).toBeDefined()
+		expect(resp1).toBeDefined()
 		//expect(resp1.trick).toContain('1')
 	});
 	test('Validando explorer 3', () => {
 		const explorer3 = {name: "Explorer3", score: 3}
 		const resp2=FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
-		expect(explorersMission).toBeDefined()
+		expect(resp2).toBeDefined()
 		//expect(resp2.trick).toContain('FIZZ')
 	});
 	test('Validando explorer 5 y 15', () => {
 		const explorer5 = {name: "Explorer5", score: 5}
 		const resp3=FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
-		expect(explorersMission).toBeDefined()
+		expect(resp3).toBeDefined()
 		//expect(resp3.trick).toContain('BUZZ')
 
 		const explorer15 = {name: "Explorer15", score: 15}
 		const resp4= FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
-		expect(explorersMission).toBeDefined()
+		expect(resp4).toBeDefined()
 		//expect(resp4.trick).toContain('FIZZBUZZ')
 	});
 })
