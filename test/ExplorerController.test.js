7903303e80558acc3ffbe819f24f290684c2f284
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-07-18 22:15:31 -0500

    creacion y maquetado de controlador, adición de express

---
+++ test/ExplorerController.test.js
@@ -0,0 +1,16 @@
+const ExplorerController = require("./../lib/controllers/ExplorerController");
+
+describe("Pruebas Unitarias para la clase ExplorerController", () =>{
+    test("Validando controlador para filtro de misión", () => {
+        const resp1=ExplorerController.getExplorersByMission("node");
+        expect(resp1).toBeDefined();
+    });
+    test("Validando controlador para usernames de explorers", () => {
+        const resp2=ExplorerController.getExplorersUsernamesByMission("node");
+        expect(resp2).toBeDefined();
+    });
+    test("Validando controlador para numero de explorers", () => {
+        const resp3=ExplorerController.getExplorersAmountByMission("node");
+        expect(resp3).toBeDefined();
+    });
+});
